32b695cbf3e89d0c3b8dc074b7e9c29f0e795932
lissandramota <lissandramota1991@gmail.com>
2020-08-07 11:54:44 -0300

    exercicio 1 de PHP

---
+++ questãoImparPar/exercicio1.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Dgite um número inteiro: " .PHP_EOL;
+
+$numero = readline("número");
+
+if ($numero %2 == 0){
+    echo "O número é par" .PHP_EOL;
+} else {
+    echo "O número é ímpar" .PHP_EOL;
+}
