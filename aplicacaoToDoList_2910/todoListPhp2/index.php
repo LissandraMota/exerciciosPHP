2bc7d2d0781dbb6e11462d9a2bd3527cd2fc0547
Lissandra Mota <56882990+LissandraMota@users.noreply.github.com>
2020-10-30 14:25:16 -0300

    exercicio php visuzalizar descricao

---
+++ aplicacaoToDoList_2910/todoListPhp2/index.php
@@ -0,0 +1,32 @@
+<?php
+// index sera o ponto central
+
+require "src/classes/Tarefa.php";
+require "src/classes/ArquivoTarefa.php";
+
+// esta linha redireciona qualquer requisição no index para o endereco
+// header('Location: /resource/lista_tarefas.html');
+
+//lendo a template html
+$template = file_get_contents('resource/lista_tarefas.html');
+
+//lendo o arquivo
+$arquivoTarefa = new ArquivoTarefa('tarefas.json');
+$listaTarefasJSON = $arquivoTarefa->le();
+
+$modeloTarefa = file_get_contents('resource/tarefa.html');
+
+$linhasTabela = '';
+
+//iteirando as tarefas
+foreach ($listaTarefasJSON as $tarefa) {
+    $tr = ''; //uma linha
+    $tr = str_replace('#STATUS', $tarefa->getStatus(), $modeloTarefa);
+    $tr = str_replace('#ID',     $tarefa->getId(), $tr);
+    $tr = str_replace('#NOME',  $tarefa->getNome(), $tr);
+    $tr = str_replace('#DATALIMITE', $tarefa->getDataLimite(), $tr);
+    $linhasTabela .= $tr;
+}
+
+echo str_replace('#TAREFAS', $linhasTabela, $template);
+// pesquise pela marcação #Tarefas, troque pelo que está na variável linhas tabelas e faz isso no template
